01b25e0112ce4699b2e82eb5a34e51e159dbdcf8
MouniaTonazzini <mounia.tonazzini@gmail.com>
2025-12-15 12:06:12 +0100

    modifications de l'app.py et du model.py

--- app.py
+++ app.py
@@ -2,61 +2,61 @@ from fastapi import FastAPI, UploadFile, File
 from fastapi.responses import JSONResponse
 from schemas import PredictionResponse, DiseasePrediction
 from PIL import Image
-from model import load_model, predict_image
+from model import load_model, preprocess_image,predict_image
 import os
 from dotenv import load_dotenv
 import uvicorn
 
 load_dotenv()
 
-PORT = os.getenv("PORT", "4000")
-MLFLOW_TRACKING_URI = os.getenv("MLFLOW_TRACKING_URI", "https://mlflow.monserveur.com")
-API_URI = os.getenv("API_URI", "http://localhost:7860")
-
 app = FastAPI(title="VitiScan Diagno API")
 
-model = load_model(mlflow_uri=MLFLOW_TRACKING_URI) # A revoir 
+model = load_model()
+# model=None # For testing the API without depending of Mlflow
 
 @app.get("/")
 def root():
     return {"message": "Vitiscan Diagno API is running"}
 
 '''
+# Réponse factice
 @app.post("/diagno", response_model=PredictionResponse)
 async def diagno(file: UploadFile = File(...)):
     if file is None:
         return JSONResponse(status_code=400, content={"message": "Aucun fichier reçu"})
     
     # Réponse factice : mock response
-    fake_prediction = "mildiou"
-    fake_confidence = 0.87
-    fake_model_version = "Resnet18_50ep_v2"
+    # fake_prediction = "mildiou"
+    # fake_confidence = 0.87
+    # fake_model_version = "Resnet18_50ep_v2"
 
+    predictions = [DiseasePrediction(disease="mildiou", confidence=0.87)]
     return PredictionResponse(
-        disease=fake_prediction,
-        confidence=fake_confidence,
-        model_version=fake_model_version
+        predictions=predictions,
+        model_version="Resnet18_50ep_v2"
     )
+
+    # return PredictionResponse(
+    #     disease=fake_prediction,
+    #     confidence=fake_confidence,
+    #     model_version=fake_model_version
+    # )
 '''
 
 @app.post("/predict", response_model=PredictionResponse)
 async def predict(file: UploadFile = File(...)):
     if file is None:
         return JSONResponse(status_code=400, content={"message": "Aucun fichier reçu"})
-    image = Image.open(file.file).convert("RGB") # Lecture de l'image
-    #image_bytes = await file.read()
-    #image = Image.open(io.BytesIO(image_bytes)).convert("RGB")
+    
+    contents = await file.read()
+    image = preprocess_image(contents)
     raw_predictions = predict_image(model, image) # Prédictions
     predictions = [DiseasePrediction(disease=d, confidence=c) for d, c in raw_predictions]
+    
     return PredictionResponse(
         predictions=predictions,
-        model_version="Resnet18_50ep_v2"
-        #disease=disease,
-        #confidence=confidence,
-        #mlflow_uri=MLFLOW_TRACKING_URI,
-        #api_uri=API_URI
-    )
-
+        model_version="vitiscan_resnet@production"
+        )
 
 if __name__ == "__main__":
     uvicorn.run("app:app", host="127.0.0.1", port=4000, reload=True)
--- model.py
+++ model.py
@@ -2,7 +2,46 @@ import mlflow
 from PIL import Image
 import torch
 import torchvision.transforms as transforms
+import io
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+
+
+# ---------------------  LOADING THE MODEL FROM MLFLOW ----------------------
+
+
+def load_model():
+    tracking_uri = os.getenv("MLFLOW_TRACKING_URI","https://mlflow.monserveur.com")
+    model_uri = os.getenv("MLFLOW_MODEL_URI")
+
+    mlflow.set_tracking_uri(tracking_uri)
+
+    model = mlflow.pytorch.load_model(model_uri)
+    model.eval()
+    return model
+
+
+# -----------------------  PREPROCESSING OF THE IMAGE ------------------------
+
+transform = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.ColorJitter(brightness=0.5, contrast=0.5),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                            std=[0.229, 0.224, 0.225])
+    ]) 
+
+def preprocess_image(file_bytes: bytes) -> torch.Tensor:
+    image = Image.open(io.BytesIO(file_bytes)).convert("RGB")
+    tensor = transform(image).unsqueeze(0)
+    return tensor
+
+
+# ---------------------------------  PREDICTION -------------------------------
+
+# Classes names
 disease = [
     "anthracnose",
     "brown_spot",
@@ -13,29 +52,6 @@ disease = [
     "shot_hole"
 ]
 
-# Chargement depuis URI direct (ex:s3)
-#def load_model():
-#    model_uri = "s3://aws-s3-mlflow/mlflow-artifacts/"
-#    model = mlflow.pytorch.load_model(model_uri)
-#    model.eval()   
-#    return model
-
-# Chargement depuis MLFlow (nom du modèle)
-def load_model(mlflow_uri: str):
-    mlflow.set_tracking_uri(mlflow_uri)
-    model = mlflow.pytorch.load_model("models:/grape_disease/latest") # remplacer par le nom du modèle
-    model.eval()
-    return model
-
-def preprocess_image(image: Image.Image):
-    transform = transforms.Compose([
-        transforms.Resize((224, 224)),
-        transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                             std=[0.229, 0.224, 0.225])
-    ])
-    return transform(image).unsqueeze(0)
-
 def predict_image(model, image: Image.Image):
     input_tensor = preprocess_image(image)
     with torch.no_grad():
@@ -43,35 +59,5 @@ def predict_image(model, image: Image.Image):
         probs = torch.nn.functional.softmax(output, dim=1)[0] 
         predictions = [(disease[i], float(probs[i])) for i in range(len(disease))] # Création de la liste DiseasePrediction
         predictions.sort(key=lambda x: x[1], reverse=True) # Tri décroissante de la confiance
-        #predictions = [
-        #    DiseasePrediction(disease=disease[i], confidence=float(probs[i]))
-        #    for i in range(len(disease))
-        #]
-        #predictions.sort(key=lambda x: x[1], reverse=True)
-        return predictions
-
-'''
-# def load_model(mlflow_uri: str):
-#     mlflow.set_tracking_uri(mlflow_uri)
-#     # Exemple : charger le dernier modèle enregistré
-#     model = mlflow.pytorch.load_model("models:/grape_disease/latest")
-#     model.eval()
-#     return model
-#
-# def preprocess_image(image: Image.Image):
-#     transform = transforms.Compose([
-#         transforms.Resize((224, 224)),
-#         transforms.ToTensor(),
-#         transforms.Normalize(mean=[0.485, 0.456, 0.406],
-#                              std=[0.229, 0.224, 0.225])
-#     ])
-#     return transform(image).unsqueeze(0)
-#
-# def predict_image(model, image: Image.Image):
-#     input_tensor = preprocess_image(image)
-#     with torch.no_grad():
-#         output = model(input_tensor)
-#         confidence, predicted_class = torch.max(torch.nn.functional.softmax(output, dim=1), 1)
-#     disease = predicted_class.item()
-#     return f"Disease_{disease}", float(confidence)
-'''
+     
+    return predictions
