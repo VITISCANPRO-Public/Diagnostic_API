2594abd7130f5567221eb8e515d930714f399944
NeiDo <inti.boulanger69@gmail.com>
2025-12-17 11:59:05 +0100

    modif app : remove ColorJiter et load_model

--- app.py
+++ app.py
@@ -20,22 +20,22 @@ load_dotenv()
 DEVICE='cpu'
 MLFLOW_TRACKING_URI = os.getenv("MLFLOW_TRACKING_URI","https://gviel-mlflow37.hf.space")
 #MLFLOW_MODEL_URI = os.getenv("MLFLOW_MODEL_URI", "mlflow-artifacts:/1/models/m-d4b6b9639a2b461b882af6c1ef3fbc61/artifacts") # modele GPU du 15/12 matin
-MLFLOW_MODEL_URI = os.getenv("MLFLOW_MODEL_URI", "mlflow-artifacts:/1/models/m-9108424091d4400bba390f1e00f7a914/artifacts") # modèle CPU du 15/12 soir
+MLFLOW_MODEL_URI = os.getenv("MLFLOW_MODEL_URI", "s3://aws-s3-mlflow/mlflow-artifacts/3/models/m-46e598be60f940849247fc01cf53dc3c/artifacts/data/model.pth") # modèle CPU du 15/12 soir
 mlflow.set_tracking_uri(MLFLOW_TRACKING_URI)
-MODEL = mlflow.pytorch.load_model(MLFLOW_MODEL_URI)
+MODEL = torch.load(MLFLOW_MODEL_URI, map_location="cpu")
 MODEL.eval()
+EXPERIMENT_NAME= os.getenv("EXPERIMENT_NAME")
 
-RUN_ID="2ac846b9752d4561ba7fa58864fec52a"
-run = mlflow.get_run(RUN_ID)
-experiment_id = run.info.experiment_id
-experiment = mlflow.get_experiment(experiment_id)
-EXPERIMENT_NAME = experiment.name
+#RUN_ID="2ac846b9752d4561ba7fa58864fec52a"
+#run = mlflow.get_run(RUN_ID)
+#experiment_id = run.info.experiment_id
+#experiment = mlflow.get_experiment(experiment_id)
+#EXPERIMENT_NAME = experiment.name
 print("EXPERIMENT_NAME=", EXPERIMENT_NAME)
 
 
 transform = transforms.Compose([
             transforms.Resize((224, 224)),
-            transforms.ColorJitter(brightness=0.5, contrast=0.5),
             transforms.ToTensor(),
             transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                     std=[0.229, 0.224, 0.225])
--- model.py
+++ model.py
@@ -3,7 +3,6 @@
 def preprocess_image(img_filename:str) -> torch.Tensor:
     transform = transforms.Compose([
             transforms.Resize((224, 224)),
-            transforms.ColorJitter(brightness=0.5, contrast=0.5),
             transforms.ToTensor(),
             transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                     std=[0.229, 0.224, 0.225])
